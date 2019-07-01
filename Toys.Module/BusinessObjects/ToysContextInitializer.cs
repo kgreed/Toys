9b98259bbc5e80b9fdca020250dd91c63aae99be
Kirsten Greed <kirsteng@jobtalk.com.au>
2019-07-02 06:08:54 +1000

    Changes from T711818

--- Toys.Module/BusinessObjects/ToysContextInitializer.cs
+++ Toys.Module/BusinessObjects/ToysContextInitializer.cs
@@ -1,11 +1,15 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using DevExpress.ExpressApp.Design;
 using DevExpress.ExpressApp.EF.DesignTime;
 
 namespace Toys.Module.BusinessObjects
 {
     public class ToysContextInitializer : DbContextTypesInfoInitializerBase {
         protected override DbContext CreateDbContext() {
-            return new ToysDbContext("App=EntityFramework");
+            // return new ToysDbContext("App=EntityFramework");
+            DbContextInfo contextInfo = new DbContextInfo(typeof(ToysDbContext), new DbProviderInfo(providerInvariantName: "System.Data.SqlClient", providerManifestToken: ""));
+            return contextInfo.CreateInstance();
         }
     }
 }
